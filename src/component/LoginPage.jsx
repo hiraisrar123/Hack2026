fd9d36ce4fe617cf1cee980ad75845aeb90b8ecb
hiraisrar123 <hiraisrar47@gmail.com>
2026-02-15 14:18:39 +0500

    12345679

--- src/component/LoginPage.jsx
+++ src/component/LoginPage.jsx
@@ -91,11 +91,11 @@ const LoginPage = () => {
               type="button"
               className="w-full flex items-center justify-center gap-2 border py-2 rounded-sm hover:bg-gray-50 transition"
             >
-              <img
+              {/* <img
                 src="https://www.svgrepo.com/show/475656/google-color.svg"
                 alt="Google"
                 className="w-5 h-5"
-              />
+              /> */}
               Sign in with Google
             </button>
           </form>
